aa43ddfb2a615e2f5ed8667307bf9c778559ebee
philippe_pras <ph.pras@gmail.com>
2022-06-29 12:45:23 +0200

    Update

--- vanilla.js
+++ vanilla.js
@@ -1,8 +1,22 @@
-window.onload = function(event) {
-    console.log("Bonjour")
-    fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score").then(function (response) {
+const fetchBestMovie = function() {
+    return fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score").then(function (response) {
         return response.json();
     }).then(function (data) {
-        console.log(data);
+        return data["results"][0];
     })
+}
+
+const displayBestMovie = function(movie) {
+    const bestVideoTitle = document.querySelector("#best-video-title");
+    bestVideoTitle.innerHTML = movie["title"];
+
+    const bestVideoImage = document.querySelector("#best-video-image");
+    bestVideoImage.setAttribute("src", movie["image_url"]);
+}
+
+window.onload = async function(event) {
+    console.log("Bonjour");
+    const bestMovie = await fetchBestMovie();
+    console.log(bestMovie);
+    displayBestMovie(bestMovie);
 }
