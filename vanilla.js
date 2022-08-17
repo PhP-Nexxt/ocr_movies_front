5a75ca64e19e66d4ba5664304901dc8529cb774a
philippe_pras <ph.pras@gmail.com>
2022-08-17 12:00:16 +0200

    Mises a jour

--- vanilla.js
+++ vanilla.js
@@ -33,57 +33,104 @@ const displayBestMovie = function(movie) {
 
 const displayGenres = function(genres) {
     const genresListSection = document.querySelector("#genres-list");
-    for (const genre in genres) {
+    console.dir(genres);
+    genres.forEach(async function(genre, index){
         console.log(genre);
         const genreArticle = document.createElement("article");
-        genreArticle.innerHTML = `<h2>${genres[genre]["name"]}</h2>`;
+        genreArticle.innerHTML = `<h2>${genre["name"]}</h2>`;
         genresListSection.append(genreArticle);
-        displayMoviesByGenre(genres[genre]["movies"], genreArticle);
-    }
+        displayMoviesByGenre(genre, genreArticle);
+    });
 }
 
-const displayMoviesByGenre = function(movies, genreArticle) {
-    const moviesList = document.createElement("div");
-    moviesList.classList.add("slider")
-    for (const movie in movies) {
-        const movieElement = document.createElement("div");
-        movieElement.classList.add("slide");
-        movieElement.innerHTML = `<img src="${movies[movie]["image_url"]}" alt="${movies[movie]["title"]}" />`;
-        movieElement.style.transform = `translateX(${movie * 100}%)`;
-        moviesList.append(movieElement);
+const displayMoviesByGenre = function(genre, genreArticle) {
+    const carouselItem = document.createElement("div");
+    carouselItem.classList.add("carousel", "js-product-carousel");
+
+    const carouselViewItem = document.createElement("div");
+    carouselViewItem.classList.add("carousel__view");
+
+    const previousItem = document.createElement("span");
+    previousItem.classList.add("carousel__control", "js-carousel-prev");
+    previousItem.innerHTML = `<i class="icon">previous</i>`;
+
+    const nextItem = document.createElement("span");
+    nextItem.classList.add("carousel__control", "js-carousel-next");
+    nextItem.innerHTML = `<i class="icon">next</i>`;
+
+    const moviesListItem = document.createElement("ul");
+    moviesListItem.classList.add("product-list", "js-product-list");
+
+    const productListWidth = 0;
+    const productAmountVisible = 3;
+    const productAmount = 0;
+
+    const moveProductList = function() {
+    moviesListItem.style.transform = `translateX(-${205*genre["step"]}px)`;
     }
-    const buttonNext = document.createElement("button");
-    buttonNext.classList.add("btn");
-    buttonNext.classList.add("btn-next")
-    buttonNext.innerHTML = "&gt;";
-    moviesList.append(buttonNext);
 
-    const buttonPrev = document.createElement("button");
-    buttonPrev.classList.add("btn");
-    buttonPrev.classList.add("btn-prev")
-    buttonPrev.innerHTML = "&lt;";
-    moviesList.append(buttonPrev);
+    previousItem.onclick = function() {
+        if(genre["step"] > 0) {
+            genre["step"]--;
+            moveProductList();
+        }
+    };
 
-    genreArticle.append(moviesList);
+    nextItem.onclick = function() {
+        console.log("next");
+        console.log(genre["step"]);
+        console.log(genre["movies"].length)
+        if(genre["step"] < genre["movies"].length-productAmountVisible) {
+            genre["step"]++;
+            moveProductList();
+        }
+    };
+   
+    
+    carouselViewItem.appendChild(previousItem);
+    carouselViewItem.appendChild(nextItem);
+
+    console.log(genre);
+    console.log(genre["movies"]);
+    for (const movie of genre["movies"]) {
+        const movieItem = document.createElement("li");
+        movieItem.classList.add("product-list__item");
+        movieItem.innerHTML = `<div class="product"><img src="${movie["image_url"]}" alt="${movie["title"]}" /></div>`;
+        moviesListItem.appendChild(movieItem);
+    }
+
+    carouselViewItem.appendChild(moviesListItem);
+    carouselItem.appendChild(carouselViewItem);
+    genreArticle.appendChild(carouselItem);
 }
 
-const displayBestMovies = function(bestMovies){
-    displayMoviesByGenre(bestMovies, document.querySelector("#best-movies"));
+
+const displayBestMovies = function(genre){
+    displayMoviesByGenre(genre, document.querySelector("#best-movies"));
 }
 window.onload = async function(event) {
     console.log("Bonjour");
     const bestMovies = await fetchBestMovies();
     const bestMovie = bestMovies[0];
     const genres = await fetchGenresList();
-    for (const genre in genres) {
-        const movies = await fetchMoviesByGenre(genres[genre]["name"]);
-        genres[genre]["movies"] = movies;
+    const genreList = [];
+    for (const genre of genres) {
+        const movies = await fetchMoviesByGenre(genre["name"]);
+        genreList.push({
+            "name": genre["name"],
+            "movies": movies,
+            "step": 0
+        });
     }
 
     console.log(bestMovie);
-    console.log(genres);
+    console.log(genreList);
     displayBestMovie(bestMovie);
-    displayBestMovies(bestMovies);
-    displayGenres(genres);
+    const bestMoviesGenre = {
+        "movies": bestMovies,
+        "step": 0
+    }
+    displayBestMovies(bestMoviesGenre);
+    displayGenres(genreList);
 }
 
